5f8deadd86b428d346bda0f7134a26417a841dc7
Bhanuvamsi2005 <bhanuvamsia@gmail.com>
2026-05-06 20:12:04 +0530

    Switch email sending to Resend API
    Refactor email sending to use Resend API instead of SMTP.

--- backend/app/utils/email.py
+++ backend/app/utils/email.py
@@ -1,26 +1,50 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+# import smtplib
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
+# import os
+
+# EMAIL = os.getenv("EMAIL")
+# PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+
+# def send_email(to, subject, html):
+#     try:
+#         msg = MIMEMultipart()
+#         msg["From"] = EMAIL
+#         msg["To"] = to
+#         msg["Subject"] = subject
+
+#         msg.attach(MIMEText(html, "html"))
+
+#         server = smtplib.SMTP("smtp.gmail.com", 587)
+#         server.starttls()
+#         server.login(EMAIL, PASSWORD)
+#         server.sendmail(EMAIL, to, msg.as_string())
+#         server.quit()
+
+#         print("✅ Email sent to:", to)
+
+#     except Exception as e:
+#         print("❌ Email error:", e)
+
+
+
+import resend
 import os
 
-EMAIL = os.getenv("EMAIL")
-PASSWORD = os.getenv("EMAIL_PASSWORD")
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 
 def send_email(to, subject, html):
+
     try:
-        msg = MIMEMultipart()
-        msg["From"] = EMAIL
-        msg["To"] = to
-        msg["Subject"] = subject
 
-        msg.attach(MIMEText(html, "html"))
-
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login(EMAIL, PASSWORD)
-        server.sendmail(EMAIL, to, msg.as_string())
-        server.quit()
+        resend.Emails.send({
+            "from": "onboarding@resend.dev",
+            "to": to,
+            "subject": subject,
+            "html": html
+        })
 
         print("✅ Email sent to:", to)
 
